7ac49276ad7f1796669ef53aef94f3980c96262d
John Ellie Go <ellie.go12@gmail.com>
2021-03-23 15:32:32 +0800

    Destini Challenge

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -14,11 +14,30 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    var storyBrain = StoryBrain()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        updateUI()
     }
 
-
+    @IBAction func choiceMade(_ sender: UIButton) {
+        guard let choice = sender.currentTitle else {
+            return
+        }
+        
+        storyBrain.choiceMade(choice)
+        updateUI()
+    }
+    
+    func updateUI() {
+        let story = storyBrain.nextStory()
+        
+        storyLabel.text = story.title
+        choice1Button.setTitle(story.choice1, for: .normal)
+        choice2Button.setTitle(story.choice2, for: .normal)
+    }
+    
 }
 
--- Destini-iOS13/Model/Story.swift
+++ Destini-iOS13/Model/Story.swift
@@ -7,3 +7,19 @@
 //
 
 import Foundation
+
+struct Story {
+    let title: String
+    let choice1: String
+    let choice1Destination: Int
+    let choice2: String
+    let choice2Destination: Int
+    
+    init(title: String, choice1: String, choice1Destination: Int, choice2: String, choice2Destination: Int) {
+        self.title = title
+        self.choice1 = choice1
+        self.choice1Destination = choice1Destination
+        self.choice2 = choice2
+        self.choice2Destination = choice2Destination
+    }
+}
